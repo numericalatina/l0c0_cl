490995175b35d685ef99dfd52ffa5117217f7e28
dansanti <dansanti@gmail.com>
2019-03-22 03:53:33 -0300

    [FIX]Uso de id en vez de objecto
    en obtención de diario 
    close #67

--- models/invoice.py
+++ models/invoice.py
@@ -1130,14 +1130,14 @@ a VAT."""))
     def _default_journal(self):
         if self._context.get('default_journal_id', False):
             return self.env['account.journal'].browse(self._context.get('default_journal_id'))
-        company_id = self._context.get('company_id', self.company_id or self.env.user.company_id)
+        company_id = self._context.get('company_id', self.company_id.id or self.env.user.company_id.id)
         if self._context.get('honorarios', False):
             inv_type = self._context.get('type', 'out_invoice')
             inv_types = inv_type if isinstance(inv_type, list) else [inv_type]
             domain = [
                 ('journal_document_class_ids.sii_document_class_id.document_letter_id.name','=','M'),
                 ('type', 'in', [TYPE2JOURNAL[ty] for ty in inv_types if ty in TYPE2JOURNAL])
-                ('company_id', '=', company_id.id),
+                ('company_id', '=', company_id),
             ]
             journal_id = self.env['account.journal'].search(domain, limit=1)
             return journal_id
@@ -1145,7 +1145,7 @@ a VAT."""))
         inv_types = inv_type if isinstance(inv_type, list) else [inv_type]
         domain = [
             ('type', 'in', [TYPE2JOURNAL[ty] for ty in inv_types if ty in TYPE2JOURNAL]),
-            ('company_id', '=', company_id.id),
+            ('company_id', '=', company_id),
         ]
         return self.env['account.journal'].search(domain, limit=1, order="sequence asc")
 
