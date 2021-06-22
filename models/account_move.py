15ce259f28d817009650e1b902a9619e7811efaa
Daniel Santibáñez Polanco <dansanti@gmail.com>
2021-06-22 12:07:48 -0400

    [FIX]Documento por defecto

--- models/account_move.py
+++ models/account_move.py
@@ -117,6 +117,12 @@ class AccountMove(models.Model):
             return True
         return False
 
+    def _default_document_class_id(self):
+        if not self.env["ir.model"].search([("model", "=", "sii.document_class")]):
+            return False
+        jdc = self._default_journal_document_class_id()
+        return jdc.sii_document_class_id.id
+
     document_class_ids = fields.Many2many(
         "sii.document_class", compute="get_dc_ids", string="Available Document Classes",
     )
@@ -129,6 +135,7 @@ class AccountMove(models.Model):
     )
     document_class_id = fields.Many2one(
         "sii.document_class", string="Document Type", readonly=True, states={"draft": [("readonly", False)]},
+        default=lambda self: self._default_document_class_id(),
     )
     sii_code = fields.Integer(
         related="document_class_id.sii_code", string="Document Code", copy=False, readonly=True, store=True,
