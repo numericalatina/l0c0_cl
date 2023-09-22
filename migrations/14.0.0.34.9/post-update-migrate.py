3fcd2c80c8d59dc3991595261b100b1bb2a4731d
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-09-22 06:08:59 -0300

    mejora migrations

--- migrations/14.0.0.34.9/post-update-migrate.py
+++ migrations/14.0.0.34.9/post-update-migrate.py
@@ -6,3 +6,6 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, installed_version):
     _logger.warning('Post Migrating l10n_cl_fe from version %s to 14.0.0.34.9' % installed_version)
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    cr.execute('''update account_move set name='/'  where use_documents is FALSE AND document_class_id IS NOT NULL move_type in ('in_invoice', 'in_refund') ''')
