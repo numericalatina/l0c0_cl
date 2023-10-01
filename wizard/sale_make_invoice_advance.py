7fb4b9a74a43fba2d907fd92efa5240efe50c5fc
Daniel Santibáñez Polanco <dansanti@gmail.com>
2023-09-30 23:47:27 -0300

    mejora wizard crear factura desde so

--- wizard/sale_make_invoice_advance.py
+++ wizard/sale_make_invoice_advance.py
@@ -16,3 +16,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
     )
     sale_order_ids = fields.Many2many(
         'sale.order', default=lambda self: self.env.context.get('active_ids'))
+
+    @api.onchange('sale_order_ids')
+    def set_referencias(self):
+        self.referencia_ids = self.env['sale.order.referencias'].search([('so_id', 'in', self.sale_order_ids.ids)])
